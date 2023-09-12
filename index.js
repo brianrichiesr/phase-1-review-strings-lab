8989f0663fcfcf706a2414ae6e4ff61068737628
Brian Richie Sr <brianrichiesr@gmail.com>
2023-09-11 21:24:28 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code in this file!
+let currentUser = "Brian Richie";
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+let excitedWelcomeMessage = welcomeMessage.toUpperCase();
+let shortGreeting = `Welcome, ${currentUser[0]}!`; 
